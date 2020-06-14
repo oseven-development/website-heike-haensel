88cb690b4df575ec071f0888fd40c92716467b9b
Maximilian Hänsel <mh@maxhaensel.de>
2020-06-14 23:20:00 +0200

    fix navigation N to n

--- gatsby/src/layout/header.tsx
+++ gatsby/src/layout/header.tsx
@@ -5,7 +5,7 @@ import React from "react"
 import { graphql, useStaticQuery, Link } from "gatsby"
 import Img from "gatsby-image"
 
-import Navigation from "./Navigation"
+import Navigation from "./navigation"
 import MobileNavigation from "./mobileNavigation"
 
 export default () => {
